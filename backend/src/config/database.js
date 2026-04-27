db375e99b8971bad8aab7c7e53d525c820686939
NguyenTienPhuong <ng.tienphuong2480@gmail.com>
2026-04-27 22:48:03 +0700

    fix sequelize config use DATABASE_URL

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,20 +1,13 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
   }
-);
+});
 
 module.exports = sequelize;
