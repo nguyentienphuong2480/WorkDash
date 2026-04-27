4b9aaf46d7ee80f3256dab9d33248ea572a22583
NguyenTienPhuong <ng.tienphuong2480@gmail.com>
2026-04-27 23:22:38 +0700

    fix sequelize config use DATABASE_URL 1

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require("sequelize");
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
